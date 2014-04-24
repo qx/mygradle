6f72e053fc28222f603e45644b2e152e65b008c5
ouyangzk1 <ouyangzk1@lenovo.com>
2014-04-24 13:26:56 +0800

    add progress style

---
+++ newproj/src/main/java/com/cfz/android/visual/activity/TesterActivity.java
@@ -0,0 +1,15 @@
+package com.cfz.android.visual.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.cfz.android.R;
+
+/**
+ * Created by Administrator on 2014/4/24.
+ */
+public class TesterActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tester);
+    }
+}
