0e3c9388b880f4f7f36206020c848662b96c707a
ouyangzk1 <ouyangzk1@lenovo.com>
2014-04-26 19:50:59 +0800

    update product detail

--- newproj/src/main/java/com/cfz/android/visual/activity/ProductDetailActivity.java
+++ newproj/src/main/java/com/cfz/android/visual/activity/ProductDetailActivity.java
@@ -2,19 +2,34 @@ package com.cfz.android.visual.activity;
 
 import android.os.Bundle;
 import com.cfz.android.R;
+import com.cfz.android.visual.customview.fancycoverflow.FancyCoverFlow;
+import com.cfz.android.visual.customview.fancycoverflow.FancyCoverFlowSampleAdapter;
 
 /**
  * Created by Administrator on 2014/4/26.
  */
 public class ProductDetailActivity extends BaseActivity {
+
+    private FancyCoverFlow fancyCoverFlow;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+
+        this.fancyCoverFlow = (FancyCoverFlow) this.findViewById(R.id.fancyCoverFlow);
+
+        this.fancyCoverFlow.setAdapter(new FancyCoverFlowSampleAdapter());
+        this.fancyCoverFlow.setUnselectedAlpha(1.0f);
+        this.fancyCoverFlow.setUnselectedSaturation(0.0f);
+        this.fancyCoverFlow.setUnselectedScale(0.5f);
+        this.fancyCoverFlow.setSpacing(50);
+        this.fancyCoverFlow.setMaxRotation(0);
+        this.fancyCoverFlow.setScaleDownGravity(0.2f);
+        this.fancyCoverFlow.setActionDistance(FancyCoverFlow.ACTION_DISTANCE_AUTO);
     }
 
     @Override
     protected int getActivityMoreImg() {
-        return R.drawable.productsinfo_button_share;
+        return super.getActivityMoreImg();
     }
 
     @Override
