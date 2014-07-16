2bee7b4acea5d453222163889be86b02f6a51aa0
ouyangzk1 <ouyangzk1@lenovo.com>
2014-07-16 13:46:35 +0800

    add muti click id

--- newproj/src/main/java/com/cfz/android/visual/activity/MoreActivity.java
+++ newproj/src/main/java/com/cfz/android/visual/activity/MoreActivity.java
@@ -1,19 +1,57 @@
 package com.cfz.android.visual.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import cn.trinea.android.common.util.ToastUtils;
 import com.cfz.android.R;
 
 /**
  * Created by Administrator on 2014/4/25.
  */
-public class MoreActivity extends BaseActivity {
+public class MoreActivity extends BaseActivity  {
+    @InjectView(R.id.explain)
+    RelativeLayout explain;
+    @InjectView(R.id.reback)
+    RelativeLayout reback;
+    @InjectView(R.id.callnum)
+    RelativeLayout callnum;
+    @InjectView(R.id.version)
+
+    RelativeLayout version;
+
     public void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_more);
         super.onCreate(savedInstanceState);
+        ButterKnife.inject(this);
     }
 
     @Override
     protected int getActivityTitle() {
         return R.string.title_more;
+    }
+
+
+    @OnClick({R.id.explain, R.id.reback, R.id.callnum, R.id.version})
+    public void submit(View view) {
+        switch (view.getId()) {
+            case R.id.explain:
+                ToastUtils.show(this, "explain");
+                break;
+            case R.id.reback:
+                ToastUtils.show(this, "reback");
+                break;
+            case R.id.callnum:
+                ToastUtils.show(this, "callnum");
+                break;
+            case R.id.version:
+                ToastUtils.show(this, "version");
+                break;
+            default:
+                break;
+        }
     }
 }
